2a6ff39df67e7a5d871771ce90c726df2949e0d0
entfane <nik77kez@gmail.com>
2025-04-03 20:40:33 +0200

    tested block

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from model import AttentionHead, MultiHeadAttention
+from model import AttentionHead, Block, MultiHeadAttention
 import torch
 
 def attention_dims_test():
@@ -15,10 +15,18 @@ def multihead_dims_test():
     sequence_len = 8
     embed_size = 16
     attn_dim_size = 4
-    output_dim_size = 4
     num_heads = 4
     x = torch.rand(sequence_len, embed_size)
     multihead_attention = MultiHeadAttention(num_heads, attn_dim_size, embed_size)
     print(multihead_attention(x))
 
-multihead_dims_test()
+def block_test():
+    sequence_len = 8
+    embed_size = 16
+    attn_dim_size = 4
+    num_heads = 4
+    x = torch.rand(sequence_len, embed_size)
+    block = Block(num_heads, attn_dim_size, embed_size)
+    print(block(x).shape)
+
+block_test()
