544db3470b5cffb173a627cb2fc191a0bb1f152e
entfane <nikitakezins@gmail.com>
2025-04-06 12:15:33 +0200

    tested attention head and encoder

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from model import AttentionHead, Block, MultiHeadAttention
+from model import AttentionHead, Block, Encoder, MultiHeadAttention
 import torch
 
 def attention_dims_test():
@@ -6,7 +6,7 @@ def attention_dims_test():
     embed_size = 16
     attn_dim_size = 4
     output_dim_size = 4
-    x = torch.rand(sequence_len, embed_size)
+    x = torch.rand(4, sequence_len, embed_size)
     attn = AttentionHead(embed_size, attn_dim_size, output_dim_size)
     output = attn(x)
     print(output.shape)
@@ -29,4 +29,16 @@ def block_test():
     block = Block(num_heads, attn_dim_size, embed_size)
     print(block(x).shape)
 
-block_test()
+def encoder_test():
+    vocab_size = 16
+    embed_size = 32
+    num_blocks = 4
+    num_heads = 4
+    attention_dim_size = 4
+    seq_len = 8
+    encoder = Encoder(vocab_size,embed_size, num_blocks, num_heads, attention_dim_size, seq_len)
+    x = torch.randint(low = 0, high = vocab_size - 1, size = (1, seq_len))
+    output = encoder(x)
+    print(output.shape)
+# attention_dims_test()
+encoder_test()
