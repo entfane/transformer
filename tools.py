42d8c924cbd3a7131785728892ff28cc6fd3d6bc
entfane <nikitakezins@gmail.com>
2025-05-09 09:25:36 +0200

    added tools

---
+++ tools.py
@@ -0,0 +1,16 @@
+import pickle
+
+def load_pickle_dict(dictionary):
+    with open(dictionary, 'rb') as f:
+        output = pickle.load(f)
+    return output
+
+def save_pickle_dict(dictionary, path):
+    with open(path, 'wb') as f:
+        pickle.dump(dictionary, f)
+
+def load_txt(path, encoding = "utf-8"):
+    with open(path, "r", encoding=encoding) as f:
+        lines = f.readlines()
+        output = ' '.join(lines)
+    return output
