c34f284703a1e4b0eacc0e2d7973df53e05f3f35
entfane <nikitakezins@gmail.com>
2025-05-11 09:45:22 +0200

    Added get_device tool

--- tools.py
+++ tools.py
@@ -1,6 +1,8 @@
 import pickle
 import torch
 
+from constants import DEFAULT_DEVICE
+
 def load_pickle_dict(dictionary):
     with open(dictionary, 'rb') as f:
         output = pickle.load(f)
@@ -19,3 +21,17 @@ def load_txt(path, encoding = "utf-8"):
 def encode_corpus(corpus, token_to_idx):
     encoded_corpus = torch.tensor([token_to_idx[token] for token in corpus], dtype = torch.long)
     return encoded_corpus
+
+def get_device(device):
+    if device == DEFAULT_DEVICE:
+        return "cuda" if torch.cuda.is_available() else "cpu"
+    elif device == "cpu":
+        return "cpu"
+    elif device == "cuda":
+        if torch.cuda.is_available():
+            return device
+        else:
+            raise Exception("Cuda is unavailable")
+    else:
+        raise Exception("Device is unavailable. Choose either 'cpu' or 'cuda' if available")
+
